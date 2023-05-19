478af99977e395085ea2d1abfa70225b8c4b6ef9
Daniel Lobo <info@danlobo.co.uk>
2023-05-19 15:05:52 +0100

    - add updated build - fix issues after npm audit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2294bef1c1c462f024ef9d53de4d4ab2.js"
+  "./precache-manifest.f409d3c1cd7cee301d17eac7b3dc2f61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
